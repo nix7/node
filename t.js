2c45d7de1528be23ff2844cea7249ce4e54e91f9
nix7 <mkveli07@gmail.com>
2021-05-12 05:50:05 +0000

    First commit

---
+++ t.js
@@ -0,0 +1,11 @@
+(function(){console.log("hello");})();
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+readline.question(`What's your name? `, name => {
+  console.log(`Hi ${name}!`)
+  readline.close()
+})
