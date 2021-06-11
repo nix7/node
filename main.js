53919bfc6817ef6942c5e10210c0f9decbedaab2
nix7 <mkveli07@gmail.com>
2021-06-10 23:12:00 -0400

    update

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ http.createServer(function (request, response) {
    response.writeHead(200, {'Content-Type': 'text/plain'});
    
    // Send the response body as "Hello World"
-   response.end('Hello World\n <h1>yeah<\h1>');
+   response.end('Hello World');
 }).listen(8081);
 
 // Console will print the message
